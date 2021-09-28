65fc125f85e72b12d51ce47b68d724db3a6c7762
Isak Horvath <isakhorvath@gmail.com>
2021-09-28 19:07:31 +0200

    NEW: Console width & height

--- source/donsole.d
+++ source/donsole.d
@@ -35,12 +35,15 @@ enum SpecialKey {
      tab = 9,
 }
 
+// Position in console
+alias Tuple!(int, "x", int, "y") Point;
+
 //START
 
-import core.sys.windows.windows, std.stdio, std.string;
+import core.sys.windows.windows, std.stdio;
 
-private enum BG = 0xf0;
-private enum FG = 0x0f;
+private enum BG_MASK = 0xf0;
+private enum FG_MASK = 0x0f;
 
 enum Color : ushort {
      black = 0,
@@ -64,7 +67,128 @@ enum Color : ushort {
      initial = 256 // Default
 }
 
+// __gshared implies static
+private __gshared {
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	HANDLE handleOutput = null, handleInput = null;
+
+	Color fg, bg, defFg, defBg;
+}
+
+private void loadDefaultColors(OutputStream os) {
+	uint handle;
+
+	if(os == OutputStream.stdout) {
+	      handle = STD_OUTPUT_HANDLE;
+	}
+	else if(os == OutputStream.stderr) {
+	      handle = STD_ERROR_HANDLE;
+	}
+	else {
+	     assert(0, "Invalid output stream");
+	}
+
+	handleOutput = GetStdHandle(handle);
+	handleInput = GetStdHandle(STD_INPUT_HANDLE);
+
+	// Get current console colors
+	GetConsoleScreenBufferInfo(handleOutput, &info);
+
+	// First 4 bits = bg
+	defBg = cast(Color)((info.wAttributes & (BG_MASK)) >> 4);
+	// Last 4 bits = fg
+	defFg = cast(Color)(info.wAttributes & (FG_MASK));
+
+	fg = Color.initial;
+	bg = Color.initial;
+}
+
+// Sets output stream
+void outputStream(OutputStream os) @property {
+     loadDefaultColors(os);
+}
+
+// Get console size
+Point size() @property {
+      GetConsoleScreenBufferInfo(handleOutput, &info);
+
+      int cols, rows;
+      cols = (info.srWindow.Right - info.srWindow.Left + 1);
+      rows = (info.srWindow.Bottom - info.srWindow.Top + 1);
+
+      return Point(cols, rows);
+}
+
+// Get cursor position
+Point cursorPos() @property {
+      GetConsoleScreenBufferInfo(handleOutput, &info);
+      return Point(info.dwCursorPosition.X, min(info.dwCursorPosition.Y, height));
+}
+
+private ushort buildColor(Color fg, Color bg) {
+	if(fg == Color.initial) {
+	      fg = defFg;
+	}
+	if(bg == Color.initial) {
+	      bg = defBg;
+	}
+	return cast(ushort)(fg | bg << 4);
+}
+
+private void updateColor() {
+	stdout.flush();
+	SetConsoleTextAttribute(handleOutput, buildColor(fg, bg));
+}
+
+// Get current console font color
+Color foreground() @property {
+      return fg;
+}
+
+// Get current console background color 
+Color background() @property {
+      return bg;
+}
+
+// Set console font color and flush stdout
+void foreground(Color color) @property {
+     fg = color;
+     updateColor();
+}
+
+// Set console background color and flush stdout
+void background(Color color) @property {
+     bg = color;
+     updateColor();
+}
+
+// Sets console cursor position
+void setCursorPosition(int x, int y) {
+     COORD coord = {
+     	   cast(short)min(width, max(0, x)),
+	   cast(short)max(0, y)
+     };
+     stdout.flush();
+     SetConsoleCursorPosition(handleOutput, coord);
+}
+
+private void moveCursor(int x, int y) {
+	stdout.flush();
+	auto pos = cursorPos();
+	setCursorPosition(max(pos.x + x, 0), max(0, pos.y + y));
+}
+
 //END
+
+// Get console width
+@property int width() {
+	return size.x;
+}
+
+// Get console height
+@property int height() {
+	return size.y;
+}
 
 void main() {
 	writeln("Console Console Console");
