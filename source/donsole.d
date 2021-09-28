08d265a360c872a27c7636b051ea39428a80d992
Isak Horvath <48546805+tavro@users.noreply.github.com>
2021-09-28 15:29:45 +0200

    NEW: Colors

--- source/donsole.d
+++ source/donsole.d
@@ -1,4 +1,4 @@
-import std.stdio;
+import std.typecons, std.algorithm;
 
 enum OutputStream {
      stdout, // Standard output
@@ -9,6 +9,11 @@ enum CloseType {
      Interrupt, // Ctrl+C
      Stop,	// Ctrl+Break
      Other	// Other
+}
+
+struct CloseEvent {
+     CloseType type;
+     bool isBlockable; // Can block close event
 }
 
 struct InputMode {
@@ -29,6 +34,37 @@ enum SpecialKey {
      escape = 27,
      tab = 9,
 }
+
+//START
+
+import core.sys.windows.windows, std.stdio, std.string;
+
+private enum BG = 0xf0;
+private enum FG = 0x0f;
+
+enum Color : ushort {
+     black = 0,
+     blue = 1,
+     green = 2,
+     cyan = 3,
+     red = 4,
+     magenta = 5,
+     yellow = 6,
+     lightGray = 7,
+     gray = 8,
+     lightBlue = 9,
+     lightGreen = 10,
+     lightCyan = 11,
+     lightRed = 12,
+     lightMagenta = 13,
+     lightYellow = 14,
+     white = 15,
+
+     bright = 8,
+     initial = 256 // Default
+}
+
+//END
 
 void main() {
 	writeln("Console Console Console");
